7e67c831783b2d7102cea9ab5f43877b3fc7492c
Jacob Dawson <jacob.dawson@me.com>
2023-01-22 00:37:44 -0500

    go about it backwards

--- sklearnClassifier.py
+++ sklearnClassifier.py
@@ -6,28 +6,37 @@
 # might need these for file manipulation:
 #import os
 #import sys
-from pathlib import Path
 
-# image stuff:
+# dataset manipulation:
 #import matplotlib.pyplot as plt
 import cv2
+from sklearn.model_selection import train_test_split
 
 # math:
 import pandas as pd
 import numpy as np
 
 # we need algorithms that can do mutlilabel classification.
-from sklearn.svm import LinearSVC # maybe set multi_class="crammer_singer"?
+#from sklearn.svm import LinearSVC # maybe set multi_class="crammer_singer"?
 
-dataDirectory = Path("galaxy-zoo-the-galaxy-challenge/images_training_rev1")
-path = list(dataDirectory.glob(r"*.jpg"))
+df = pd.read_csv("galaxy-zoo-the-galaxy-challenge/training_solutions_rev1/training_solutions_rev1.csv")
+df_train, df_test = train_test_split(df, test_size=.999)
+#print(df_train.shape)
+
+dataDirectory = "galaxy-zoo-the-galaxy-challenge/images_training_rev1/"
 imgs = []
-for file in path:
-    img = cv2.imread(file.__str__(),0)
+ids = df_train.values[:,0].astype(int).astype(str)
+y_batch = df_train.values[:,1:]
+for id in ids:
+    img = cv2.imread(dataDirectory+id+'.jpg',cv2.IMREAD_COLOR)
     img = cv2.resize(img,(53,53)) # the largest factor of 424 is 53
     imgs.append(img)
-    if (len(imgs) >= 100):
-        break
+imgs = np.array(imgs)
+#print(imgs.shape)
 
-y_data = pd.read_csv("galaxy-zoo-the-galaxy-challenge/training_solutions_rev1/training_solutions_rev1.csv")
-print(y_data.axes)
+'''
+figure = plt.figure(figsize=(8,8))
+plt.xlabel(imgs[0].shape)
+plt.imshow(imgs[0])
+plt.savefig('myfilename.png', dpi=100)
+'''
