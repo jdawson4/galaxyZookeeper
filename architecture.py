3042692a1d4f3fe797ec56f358463460ac178843
Jacob Dawson <jacob.dawson@me.com>
2023-01-22 17:32:29 -0500

    more like the paper

--- architecture.py
+++ architecture.py
@@ -16,29 +16,29 @@ def layer(input, filters, size, stride, apply_batchnorm=True):
 
 # reduce the size of the image by half and only give a depth of filters
 def downsample(input, filters, apply_batchnorm=True):
-    out = keras.layers.Conv2D(filters, kernel_size=4, strides=2, padding='same')(input)
+    out = keras.layers.Conv2D(filters, kernel_size=1, strides=1, padding='same')(input)
     if apply_batchnorm:
         out = keras.layers.BatchNormalization()(out)
-    return keras.layers.Activation('selu')(out)
+    out = keras.layers.Activation('selu')(out)
+    return keras.layers.AveragePooling2D()(out)
+
 
 # link the above layers into a dense block!
 def denseBlock(input, filters, size, apply_batchnorm=True):
     # how many layers do we want per dense block?
     l1 = layer(input, filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
     l2 = layer(keras.layers.Concatenate()([input,l1]), filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
-    #l3 = layer(keras.layers.Concatenate()([input,l1,l2]), filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
-    #l4 = layer(keras.layers.Concatenate()([input,l1,l2,l3]), filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
-    #l5 = layer(keras.layers.Concatenate()([input,l1,l2,l3,l4]), filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
+    l3 = layer(keras.layers.Concatenate()([input,l1,l2]), filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
+    l4 = layer(keras.layers.Concatenate()([input,l1,l2,l3]), filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
+    l5 = layer(keras.layers.Concatenate()([input,l1,l2,l3,l4]), filters=filters, size=size, stride=1, apply_batchnorm=apply_batchnorm)
 
-    return downsample(l2, filters=filters, apply_batchnorm=apply_batchnorm)
+    return downsample(l5, filters=filters, apply_batchnorm=apply_batchnorm)
 
 def convNet():
     input = keras.layers.Input(shape=(image_size,image_size,num_channels), dtype=tf.float16)
     scale = keras.layers.Rescaling(1.0/255.0, offset=0)(input)
     out = denseBlock(scale, filters=4, size=3, apply_batchnorm=False)
-    out = denseBlock(out, filters=8, size=3, apply_batchnorm=True)
     out = denseBlock(out, filters=16, size=3, apply_batchnorm=True)
-    out = denseBlock(out, filters=32, size=3, apply_batchnorm=True)
     out = denseBlock(out, filters=64, size=3, apply_batchnorm=True)
     #out = keras.layers.Flatten()(out)
     out = keras.layers.GlobalAveragePooling2D()(out)
--- constants.py
+++ constants.py
@@ -4,11 +4,11 @@
 seed = 7
 dataDirectory = "galaxy-zoo-the-galaxy-challenge/images_training_rev1/"
 trainingCsv = "galaxy-zoo-the-galaxy-challenge/training_solutions_rev1/training_solutions_rev1.csv"
-batch_size = 128
+batch_size = 32
 image_size = 424 # natively 424
 augmentFlag = False
 bufferSize = 1000
 num_channels = 3
 output_options = 37
 learnRate = 0.003 # adam's default is 0.001
-epochs = 5
+epochs = 10
