7ab217f5c3dcfeac8e4c7579c76b2269ea96ae4a
Jacob Dawson <jacob.dawson@me.com>
2023-01-22 00:49:53 -0500

    work for the night

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Author: Jacob Dawson
+#
+# Consts we're using in places
+seed = 7
+dataDirectory = "galaxy-zoo-the-galaxy-challenge/images_training_rev1/"
+trainingCsv = "galaxy-zoo-the-galaxy-challenge/training_solutions_rev1/training_solutions_rev1.csv"
--- sklearnClassifier.py
+++ sklearnClassifier.py
@@ -3,9 +3,12 @@
 # Goal: play with the dataset using some simpler machine learning algorithms,
 # to get a feel for the problem, before we make a neural net solution.
 
+from constants import *
+
 # might need these for file manipulation:
 #import os
 #import sys
+#from pathlib import Path
 
 # dataset manipulation:
 #import matplotlib.pyplot as plt
@@ -17,13 +20,11 @@ import pandas as pd
 import numpy as np
 
 # we need algorithms that can do mutlilabel classification.
-#from sklearn.svm import LinearSVC # maybe set multi_class="crammer_singer"?
+from sklearn.svm import LinearSVC # maybe set multi_class="crammer_singer"?
 
-df = pd.read_csv("galaxy-zoo-the-galaxy-challenge/training_solutions_rev1/training_solutions_rev1.csv")
-df_train, df_test = train_test_split(df, test_size=.999)
-#print(df_train.shape)
+df = pd.read_csv(trainingCsv)
+df_train, df_test = train_test_split(df, test_size=.999) # silly test size for now
 
-dataDirectory = "galaxy-zoo-the-galaxy-challenge/images_training_rev1/"
 imgs = []
 ids = df_train.values[:,0].astype(int).astype(str)
 y_batch = df_train.values[:,1:]
@@ -32,7 +33,6 @@ for id in ids:
     img = cv2.resize(img,(53,53)) # the largest factor of 424 is 53
     imgs.append(img)
 imgs = np.array(imgs)
-#print(imgs.shape)
 
 '''
 figure = plt.figure(figsize=(8,8))
@@ -40,3 +40,10 @@ plt.xlabel(imgs[0].shape)
 plt.imshow(imgs[0])
 plt.savefig('myfilename.png', dpi=100)
 '''
+
+classifier = LinearSVC(
+    max_iter=100,
+    class_weight='balanced',
+    random_state=seed
+)
+#classifier.fit(X=imgs, y=y_batch) # need to manipulate y_batch?
